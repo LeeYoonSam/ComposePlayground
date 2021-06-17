a7061a1e3ede8035beba91c0750423868c47fa8d
YS <yoonsam86@gmail.com>
2021-06-17 16:38:55 +0900

    샘플리스트 구현

--- app/src/main/java/com/ys/composeplayground/MainActivity.kt
+++ app/src/main/java/com/ys/composeplayground/MainActivity.kt
@@ -3,11 +3,20 @@ package com.ys.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ys.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +26,7 @@ class MainActivity : ComponentActivity() {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    ComposeList()
                 }
             }
         }
@@ -25,14 +34,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeList() {
+
+    val sampleList = getComposeSampleList()
+
+    LazyColumn(
+        modifier = Modifier.fillMaxHeight()
+    ) {
+        items(
+            items = sampleList,
+            itemContent = { item ->
+                Card(
+                    elevation = 4.dp,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.padding(4.dp),
+                        text = item,
+                        style = TextStyle(fontSize = 24.sp)
+                    )
+                }
+            }
+        )
+    }
 }
+
+fun getComposeSampleList() = listOf(
+    "Crossfade",
+    "BaseTextField",
+    "Canvas",
+    "Image",
+    "LazyColumn"
+)
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePlaygroundTheme {
-        Greeting("Android")
+        ComposeList()
     }
 }
