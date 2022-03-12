e12f11890a76d68df0b4f9641844802525058cbb
YS <yoonsam86@gmail.com>
2022-03-12 18:05:39 +0900

    MainActivity BackHandler 로 뒤로가기 2번 누를때 앱종료 처리

--- app/src/main/java/com/ys/composeplayground/MainActivity.kt
+++ app/src/main/java/com/ys/composeplayground/MainActivity.kt
@@ -2,31 +2,45 @@ package com.ys.composeplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ys.composeplayground.extensions.toast
 import com.ys.composeplayground.ui.album.AlbumActivity
 import com.ys.composeplayground.ui.foundation.FoundationActivity
 import com.ys.composeplayground.ui.navigation.BottomNavigationAnimationActivity
 import com.ys.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var backPressed = 0L
+
+    private val finish: () -> Unit = {
+        if (backPressed + 1500 > System.currentTimeMillis()) {
+            finishAndRemoveTask()
+        } else {
+            toast(getString(R.string.app_exit_label))
+        }
+        backPressed = System.currentTimeMillis()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    ComposeList {
+                    ComposeList(finish) {
                         routeToActivity(it)
                     }
                 }
@@ -54,9 +68,15 @@ class MainActivity : ComponentActivity() {
     }
     @OptIn(ExperimentalMaterialApi::class)
     @Composable
-    fun ComposeList(navigateToActivity: (Int) -> Unit) {
-
+    fun ComposeList(
+        finish: () -> Unit,
+        navigateToActivity: (Int) -> Unit
+    ) {
         val sampleList = getComposeSampleList()
+
+        BackHandler {
+            finish()
+        }
 
         LazyColumn(
             modifier = Modifier.fillMaxHeight()
@@ -103,8 +123,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun DefaultPreview() {
         ComposePlaygroundTheme {
-            ComposeList {}
+            ComposeList(finish) {}
         }
     }
-
 }
