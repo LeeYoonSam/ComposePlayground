bb449329f326249934712a73324574a9362488c7
YS <yoonsam86@gmail.com>
2022-03-14 23:13:31 +0900

    Main 구조 개선 카테고리별 Compose Demo 표시

---
+++ app/src/main/java/com/ys/composeplayground/Demos.kt
@@ -0,0 +1,53 @@
+package com.ys.composeplayground
+
+import com.ys.composeplayground.ui.album.AlbumActivity
+import com.ys.composeplayground.ui.animation.CrossfadeDemo
+import com.ys.composeplayground.ui.foundation.*
+import com.ys.composeplayground.ui.material.AlertDialogSample
+import com.ys.composeplayground.ui.navigation.BottomNavigationAnimationActivity
+
+val Animation = DemoCategory(
+    "Animations",
+    listOf(
+        ComposableDemo("Crossfade") { CrossfadeDemo() },
+    )
+)
+
+val FoundationDemos = DemoCategory(
+    "Foundations",
+    listOf(
+        ComposableDemo("BaseTextField") { BaseTextField() },
+        ComposableDemo("Canvas") { CanvasDrawExample() },
+        ComposableDemo("Image") { ImageResourceDemo() },
+        ComposableDemo("LazyColumn") { LazyColumnDemo() },
+        ComposableDemo("LazyRow") { LazyRowDemo() },
+        ComposableDemo("LazyVerticalGrid") { LazyVerticalGridDemo() },
+        ComposableDemo("Shape") { ShapeDemo() },
+        ComposableDemo("Text") { TextDemo() },
+    )
+)
+
+val MaterialDemos = DemoCategory(
+    "Materials",
+    listOf(
+        ComposableDemo("AlertDialog") { AlertDialogSample() },
+    )
+)
+
+val ActivityDemos = DemoCategory(
+    "Activities",
+    listOf(
+        ActivityDemo("Album", AlbumActivity::class),
+        ActivityDemo("BottomNavigationAnimationActivity", BottomNavigationAnimationActivity::class),
+    )
+)
+
+val AllDemosCategory = DemoCategory(
+    "Jetpack Compose Playground Demos",
+    listOf(
+        Animation,
+        FoundationDemos,
+        MaterialDemos,
+        ActivityDemos,
+    )
+)
--- app/src/main/java/com/ys/composeplayground/MainActivity.kt
+++ app/src/main/java/com/ys/composeplayground/MainActivity.kt
@@ -1,25 +1,26 @@
 package com.ys.composeplayground
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.ys.composeplayground.extensions.toast
-import com.ys.composeplayground.ui.DemoComposableActivity
-import com.ys.composeplayground.ui.album.AlbumActivity
-import com.ys.composeplayground.ui.foundation.FoundationActivity
-import com.ys.composeplayground.ui.navigation.BottomNavigationAnimationActivity
 import com.ys.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,96 +38,199 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+
+        ComposeView(this).also {
+            setContentView(it)
+        }.setContent {
+            val activityStarter = fun(demo: ActivityDemo<*>) {
+                startActivity(Intent(this, demo.activityClass.java))
+            }
+
+            val navigator = rememberSaveable(
+                saver = Navigator.Saver(AllDemosCategory, onBackPressedDispatcher, activityStarter, finish)
+            ) {
+                Navigator(AllDemosCategory, onBackPressedDispatcher, activityStarter, finish)
+            }
+
             ComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    ComposeList(finish) {
-                        routeToActivity(it)
+                DemoApp(
+                    currentDemo = navigator.currentDemo,
+                    backStackTitle = navigator.backStackTitle,
+                    onNavigateToDemo = { demo ->
+                        navigator.navigateTo(demo)
                     }
-                }
+                )
             }
         }
     }
 
-    private fun routeToActivity(position: Int) {
-        when (position) {
-            0 -> startActivity(AlbumActivity.newIntent(this))
-            1 -> {}
-            2 -> startActivity(FoundationActivity.newIntent(this, 0))
-            3 -> startActivity(FoundationActivity.newIntent(this, 1))
-            4 -> startActivity(FoundationActivity.newIntent(this, 2))
-            5 -> startActivity(FoundationActivity.newIntent(this, 3))
-            6 -> startActivity(FoundationActivity.newIntent(this, 4))
-            7 -> startActivity(FoundationActivity.newIntent(this, 5))
-            8 -> startActivity(FoundationActivity.newIntent(this, 6))
-            9 -> startActivity(FoundationActivity.newIntent(this, 7))
-            10 -> startActivity(BottomNavigationAnimationActivity.newIntent(this))
-            11 -> startActivity(DemoComposableActivity.newIntent(this))
-            else -> {
 
-            }
-        }
-    }
-    @OptIn(ExperimentalMaterialApi::class)
     @Composable
-    fun ComposeList(
-        finish: () -> Unit,
-        navigateToActivity: (Int) -> Unit
+    fun DemoApp(
+        currentDemo: Demo,
+        backStackTitle: String,
+        onNavigateToDemo: (Demo) -> Unit,
     ) {
-        val sampleList = getComposeSampleList()
+        Scaffold(
+            topBar = {
+                DemoAppBar(
+                    title = backStackTitle,
+                )
+            }
+        ) { innerPadding ->
+            val modifier = Modifier.padding(innerPadding)
+            DemoContent(modifier, currentDemo, onNavigateToDemo)
+        }
+    }
 
-        BackHandler {
-            finish()
+    @Composable
+    private fun DemoContent(
+        modifier: Modifier,
+        currentDemo: Demo,
+        onNavigate: (Demo) -> Unit
+    ) {
+        Crossfade(targetState = currentDemo) { demo ->
+            Surface(modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                DisplayDemo(demo, onNavigate)
+            }
+        }
+    }
+
+    @Composable
+    private fun DisplayDemo(demo: Demo, onNavigate: (Demo) -> Unit) {
+        when (demo) {
+            is ActivityDemo<*> -> {
+                /* should never get here as activity demos are not added to the backstack*/
+            }
+            is ComposableDemo -> demo.content()
+            is DemoCategory -> DisplayDemoCategory(demo, onNavigate)
+        }
+    }
+
+    @Composable
+    @OptIn(ExperimentalMaterialApi::class)
+    private fun DisplayDemoCategory(category: DemoCategory, onNavigate: (Demo) -> Unit) {
+        Column(Modifier.verticalScroll(rememberScrollState())) {
+            category.demos.forEach { demo ->
+                ListItem(
+                    text = {
+                        Text(
+                            modifier = Modifier.height(56.dp)
+                                .wrapContentSize(Alignment.Center),
+                            text = demo.title
+                        )
+                    },
+                    modifier = Modifier.clickable { onNavigate(demo) }
+                )
+            }
+        }
+    }
+
+    @Suppress("ComposableLambdaParameterNaming", "ComposableLambdaParameterPosition")
+    @Composable
+    private fun DemoAppBar(
+        title: String,
+    ) {
+        TopAppBar(
+            title = {
+                Text(title, Modifier.testTag(Tags.AppBarTitle))
+            }
+        )
+    }
+
+    private class Navigator private constructor(
+        private val backDispatcher: OnBackPressedDispatcher,
+        private val launchActivityDemo: (ActivityDemo<*>) -> Unit,
+        private val rootDemo: Demo,
+        initialDemo: Demo,
+        private val backStack: MutableList<Demo>,
+        private val finish: () -> Unit
+    ) {
+        constructor(
+            rootDemo: Demo,
+            backDispatcher: OnBackPressedDispatcher,
+            launchActivityDemo: (ActivityDemo<*>) -> Unit,
+            finish: () -> Unit,
+        ) : this(backDispatcher, launchActivityDemo, rootDemo, rootDemo, mutableListOf<Demo>(), finish)
+
+        private val onBackPressed = object : OnBackPressedCallback(false) {
+            override fun handleOnBackPressed() {
+                if (!isRoot) {
+                    popBackStack()
+                    return
+                }
+
+                finish()
+            }
+        }.apply {
+            isEnabled = true
+            backDispatcher.addCallback(this)
         }
 
-        LazyColumn(
-            modifier = Modifier.fillMaxHeight()
-        ) {
-            items(
-                items = sampleList,
-                itemContent = { item ->
-                    Card(
-                        onClick = { navigateToActivity(item.position) },
-                        elevation = 4.dp,
-                        modifier = Modifier.padding(8.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(4.dp),
-                            text = item.menuName,
-                            style = TextStyle(fontSize = 24.sp)
-                        )
+        private var _currentDemo by mutableStateOf(initialDemo)
+        var currentDemo: Demo
+            get() = _currentDemo
+            private set(value) {
+                _currentDemo = value
+                onBackPressed.isEnabled = true
+            }
+
+        val isRoot: Boolean get() = backStack.isEmpty()
+
+        val backStackTitle: String
+            get() = (backStack.drop(1) + currentDemo).joinToString(separator = " > ") { it.title }
+
+        fun navigateTo(demo: Demo) {
+            if (demo is ActivityDemo<*>) {
+                launchActivityDemo(demo)
+            } else {
+                backStack.add(currentDemo)
+                currentDemo = demo
+            }
+        }
+
+        fun popAll() {
+            if (!isRoot) {
+                backStack.clear()
+                currentDemo = rootDemo
+            }
+        }
+
+        private fun popBackStack() {
+            currentDemo = backStack.removeAt(backStack.lastIndex)
+        }
+
+        companion object {
+            fun Saver(
+                rootDemo: DemoCategory,
+                backDispatcher: OnBackPressedDispatcher,
+                launchActivityDemo: (ActivityDemo<*>) -> Unit,
+                finish: () -> Unit
+            ): Saver<Navigator, *> = listSaver<Navigator, String>(
+                save = { navigator ->
+                    (navigator.backStack + navigator.currentDemo).map { it.title }
+                },
+                restore = { restored ->
+                    require(restored.isNotEmpty())
+                    val backStack = restored.mapTo(mutableListOf()) {
+                        requireNotNull(findDemo(rootDemo, it))
                     }
+                    val initial = backStack.removeAt(backStack.lastIndex)
+                    Navigator(backDispatcher, launchActivityDemo, rootDemo, initial, backStack, finish)
                 }
             )
-        }
-    }
 
-    fun getComposeSampleList() = listOf(
-        Menu(0, "Album"),
-        Menu(1, "Crossfade"),
-        Menu(2, "BaseTextField"),
-        Menu(3, "Canvas"),
-        Menu(4, "Image"),
-        Menu(5, "LazyColumn"),
-        Menu(6, "LazyRow"),
-        Menu(7, "LazyVerticalGrid"),
-        Menu(8, "Shape"),
-        Menu(9, "Text"),
-        Menu(10, "BottomNavigationAnimation"),
-        Menu(11, "Material"),
-    )
-
-    data class Menu(
-        val position: Int,
-        val menuName: String
-    )
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        ComposePlaygroundTheme {
-            ComposeList(finish) {}
+            private fun findDemo(demo: Demo, title: String): Demo? {
+                if (demo.title == title) {
+                    return demo
+                }
+                if (demo is DemoCategory) {
+                    demo.demos.forEach { child ->
+                        findDemo(child, title)?.let { return it }
+                    }
+                }
+                return null
+            }
         }
     }
 }
