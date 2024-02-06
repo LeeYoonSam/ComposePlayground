2999d27ff1ef1e6c23b97ffdd2e0554bca3f1c30
YS <yoonsam86@gmail.com>
2024-02-06 12:56:44 +0900

    Add Profile Component

--- app/src/main/java/com/ys/composeplayground/ui/sample/photoapp/MainScreen.kt
+++ app/src/main/java/com/ys/composeplayground/ui/sample/photoapp/MainScreen.kt
@@ -2,21 +2,26 @@ package com.ys.composeplayground.ui.sample.photoapp
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ys.composeplayground.ui.sample.photoapp.feed.Feed
+import com.ys.composeplayground.ui.sample.photoapp.profile.Profile
 
 @Composable
 fun MainScreen() {
     StatusBarColorProvider()
-    Surface(color = MaterialTheme.colorScheme.onSurface) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.onPrimaryContainer
+    ) {
         val viewModel = viewModel<PhotographersViewModel>()
         var selectedId by rememberSaveable { mutableStateOf<String?>(null) }
         Crossfade(targetState = selectedId, label = "") { id ->
@@ -26,7 +31,7 @@ fun MainScreen() {
                     onSelected = { selectedId = it.id }
                 )
             } else {
-                Text(text = "Profile - ${viewModel.getById(id)}")
+                Profile(viewModel.getById(id))
                 BackHandler {
                     selectedId = null
                 }
---
+++ app/src/main/java/com/ys/composeplayground/ui/sample/photoapp/profile/Profile.kt
@@ -0,0 +1,34 @@
+package com.ys.composeplayground.ui.sample.photoapp.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ys.composeplayground.ui.sample.photoapp.Photographer
+
+@Composable
+fun Profile(photographer: Photographer, modifier: Modifier = Modifier) {
+    val padding = 16.dp
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.onSurface,
+        contentColor = MaterialTheme.colorScheme.surface,
+    ) {
+        TutorialOverlay { tutorialHighlightModifier ->
+            Column(Modifier.padding(top = 24.dp)) {
+                Text(text = "Profile - $photographer")
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = "Profile Header")
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = "Tag List")
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = "PortfolioCard")
+            }
+        }
+    }
+}
