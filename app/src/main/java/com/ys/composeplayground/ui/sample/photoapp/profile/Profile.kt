1051e3fb60d92cf8e54d9dd4861d92af34fecf3e
YS <yoonsam86@gmail.com>
2024-02-08 14:13:04 +0900

    PhotoApp - Add TagList

--- app/src/main/java/com/ys/composeplayground/ui/sample/photoapp/profile/Profile.kt
+++ app/src/main/java/com/ys/composeplayground/ui/sample/photoapp/profile/Profile.kt
@@ -1,8 +1,16 @@
 package com.ys.composeplayground.ui.sample.photoapp.profile
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -26,12 +34,38 @@ fun Profile(photographer: Photographer, modifier: Modifier = Modifier) {
                     photographer = photographer,
                     tutorialHighlightModifier = tutorialHighlightModifier
                 )
-                Text(text = "Profile Header")
                 Spacer(modifier = Modifier.weight(1f))
-                Text(text = "Tag List")
+                TagList(
+                    tags = photographer.tags,
+                    modifier = Modifier.padding(vertical = padding)
+                )
                 Spacer(modifier = Modifier.weight(1f))
                 Text(text = "PortfolioCard")
             }
+        }
+    }
+}
+
+@Composable
+private fun TagList(tags: List<String>, modifier: Modifier = Modifier) {
+    val padding = 8.dp
+    LazyRow(
+        modifier = modifier,
+        contentPadding = PaddingValues(start = padding * 2, end = padding),
+        horizontalArrangement = Arrangement.spacedBy(padding),
+    ) {
+        items(tags) {
+            Text(
+                text = it,
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        color = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+                        shape = CircleShape
+                    )
+                    .padding(padding)
+            )
         }
     }
 }
